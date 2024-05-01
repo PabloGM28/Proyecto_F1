32022e7e0ac40eccd9571c2a0a9e3c890c4eb4f3
PabloGM28 <pgarciamaya@gmail.com>
2024-05-01 18:07:59 +0200

    Ultima versión
    Todo menos Create Your Own Circuit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('downforce.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
