e0834b7d33a785d1383770de4d85732ceb11293c
PabloGM28 <pgarciamaya@gmail.com>
2024-05-01 12:19:23 +0200

    Ultimo cambio
    Ya están listos todos los menús de los parámetros del coche

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('index.html')
-
-
+    return render_template('downforce.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
