887bf5244862a4e60d1b36adc01d1dddc7b8502d
josefelipeserna <jose.felipe.serna@alumnos.upm.es>
2024-05-06 16:09:39 +0200

    Revert "Merge branch 'main' of https://github.com/PabloGM28/Proyecto_F1"
    This reverts commit 38e2f20eefcd4ea0168335d41c18bc811bfc4e43, reversing
    changes made to 99ca61e0bc24e5d819324c388f56dff8d8428029.

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('length.html')
-    
+    return render_template('index.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
