81a63bac0b38dd407f9e7534418e5c9c616af0ad
PabloGM28 <pgarciamaya@gmail.com>
2024-05-07 09:30:33 +0200

    Ultima versión

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('length.html')
+    return render_template('weather.html')
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
