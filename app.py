723a3375f33e7c240f3ab735756cf56ddd2005e3
PabloGM28 <pgarciamaya@gmail.com>
2024-05-11 18:57:57 +0200

    Ultima versión
    Ya se puede navegar.
    Aún no recoge datos

--- app.py
+++ app.py
@@ -3,9 +3,32 @@ from flask import Flask,redirect,url_for,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
+    return render_template('index.html')
+
+@app.route('/tyres')
+def tyres():
+    return render_template('tyres.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/length')
+def length():
     return render_template('length.html')
-    
+
+@app.route('/final_strategy')
+def final_strategy():
+    return render_template('final_strategy.html')
+
+@app.route('/downforce')
+def downforce():
+    return render_template('downforce.html')
+
+@app.route('/corners')
+def corners():
+    return render_template('corners.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
