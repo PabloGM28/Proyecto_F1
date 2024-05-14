36b096171589db3b0323272b56a3f3f5c28fe343
josefelipeserna <jose.felipe.serna@alumnos.upm.es>
2024-05-14 17:12:32 +0200

    limpieza

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 import degradacion
 import tyre
 import numpy as np
+import combinatoria
 laps = 57
 vida_esperada = [12,25,30]
 degradacion_por_tipos = degradacion.degradation(43,35,4,3,5,'medium',6,0.3)
@@ -21,27 +22,6 @@ for i in (0,1,2):
 
 from itertools import combinations_with_replacement
 
-def generate_combinations(strings):
-    # Genera todas las combinaciones posibles de los strings en 4 posiciones
-    combinations = list(combinations_with_replacement(strings, 4))
-
-    # Descarta combinaciones con los 4 strings iguales
-    combinations = [comb for comb in combinations if len(set(comb)) > 1]
-
-    # Descarta combinaciones con la misma cantidad de un tipo de string
-    filtered_combinations = []
-    for comb in combinations:
-        counts = {s: comb.count(s) for s in set(comb)}
-        if len(set(counts.values())) == len(counts):
-            filtered_combinations.append(comb)
-
-    return filtered_combinations
-
-# Ejemplo de uso
-strings = ["soft", "medium", "high"]
-combinations = generate_combinations(strings)
-for comb in combinations:
-    print(comb)
 
 
 
