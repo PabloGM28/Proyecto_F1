abacdf543d3b2ffe78fd790228ec033336a4c0b2
josefelipeserna <jose.felipe.serna@alumnos.upm.es>
2024-05-20 13:45:28 +0200

    limpieza

--- circuits.py
+++
@@ -1,7 +0,0 @@
-albert_park= {"high_speed":9,"medium_speed":4,"low_speed":3,"length":5.303}
-bahrain = {"high_speed":9,"medium_speed":4,"low_speed":2,"length":5.412}
-jeddah= {"high_speed":16,"medium_speed":9,"low_speed":2,"length":6.175}
-interlagos= {"high_speed":6,"medium_speed":9,"low_speed":0,"length":4.309}
-spa= {"high_speed":7,"medium_speed":9,"low_speed":3,"length":7.004}
-suzuka= {"high_speed":11,"medium_speed":4,"low_speed":3,"length":5.807}
-new_track= {"high_speed":1,"medium_speed":1,"low_speed":1,"length":1}
